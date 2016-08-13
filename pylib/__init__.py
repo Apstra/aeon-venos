feaf9b449de746856a62e4d139af615376661799
Jermey Schulman <jeremy@apstrktr.com>
2016-08-13 12:27:45 -0400

    added license header

--- pylib/__init__.py
+++ pylib/__init__.py
@@ -1,1 +1,6 @@
+# Copyright 2014-present, Apstra, Inc. All rights reserved.
+#
+# This source code is licensed under End User License Agreement found in the
+# LICENSE file at http://www.apstra.com/community/eula
+
 __import__('pkg_resources').declare_namespace('aeon')
