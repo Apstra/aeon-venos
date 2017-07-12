bb81f1991a5115db5d797116264c0567c8c6dd4a
Bobby Watson <bobbywatson3@users.noreply.github.com>
2017-07-12 11:56:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ packages = find_packages(libdir)
 
 setup(
     name="aeon-venos",
-    version="0.7.0",
+    version="0.8.0",
     author="Jeremy Schulman",
     url='https://github.com/Apstra/aeon-venos',
     author_email="jeremy@apstra.com",
