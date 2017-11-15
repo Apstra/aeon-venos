b5c96ca96ee444672e215801a01ab3c897fd4340
Bobby Watson <bobbywatson3@users.noreply.github.com>
2017-11-15 15:59:40 -0500

    Enable nxos-installos syslog (#35)
    * Added ability to log to syslog for nxos-installos script
    
    * uprev setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ packages = find_packages(libdir)
 
 setup(
     name="aeon-venos",
-    version="0.9.6",
+    version="0.9.7",
     author="Jeremy Schulman",
     url='https://github.com/Apstra/aeon-venos',
     author_email="jeremy@apstra.com",
