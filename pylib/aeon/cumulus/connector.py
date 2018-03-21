674414e5885a96ed48a0a217b8db3d244360b220
Bobby Watson <bobbywatson3@users.noreply.github.com>
2018-03-21 15:42:36 -0400

    Fix bug when SSH cached key is different than expected (#45)
    * Don't load system keys for ubuntu/cumulus/opx

--- pylib/aeon/cumulus/connector.py
+++ pylib/aeon/cumulus/connector.py
@@ -22,7 +22,6 @@ class Connector(object):
         self.passwd = kwargs.get('passwd', 'admin')
 
         self._client = paramiko.SSHClient()
-        self._client.load_system_host_keys()
         self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
         self.open()
